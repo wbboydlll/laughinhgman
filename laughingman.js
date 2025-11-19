16963e7a20302a85fac54f7a96e9adcf006077cd
wbboydlll <wbboydlll@gmail.com>
2025-11-19 10:12:03 -0500

    added alert on square

---
+++ laughingman.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let squareElement = document.getElementById("square");
+    squareElement.addEventListener("click", ()=>{
+        alert("OMG YOU CAUGHT ME!")
+    });
+});
