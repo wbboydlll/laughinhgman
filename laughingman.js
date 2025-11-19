57a671896a3e3c6b958bb215227d2a892563c68e
wbboydlll <wbboydlll@gmail.com>
2025-11-19 10:39:05 -0500

    started loop for childern

--- laughingman.js
+++ laughingman.js
@@ -1,6 +1,30 @@
+const SQUARE_COUNT = 3;
+const SPEED = 5;
+const TIMER_SPEED = 100;
+
 document.addEventListener('DOMContentLoaded', function(){
     let squareElement = document.getElementById("square");
     squareElement.addEventListener("click", ()=>{
         alert("OMG YOU CAUGHT ME!")
     });
+
+    let box = document.querySelector("#box");
+
+    for(let i = 0;i<SQUARE_COUNT;i++)
+    {
+    //create the square
+    let square = document.createElement('img');
+    square.src = 'laughing_man.jpg';
+    square.alt = 'Catch the Laughing Man';
+    square.className = 'square';
+    box.appendChild(square);
+    //add it to the screen
+    }
+    let allSquares = box.children;
+    let squareArray = Array.from(children);
+    squareArray.forEach(function(element, index){
+        var dx = SPEED * (Math.random()*2 -1);
+        var dy = SPEED * (Math.random()*2 -1);
+    });
+    
 });
